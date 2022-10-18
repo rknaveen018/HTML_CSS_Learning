5b753a6f0c536f164114ae9d1fc005777dfec92f
Naveenkumar R K <rknaveen018@gmail.com>
2022-10-18 19:28:32 +0530

    Adding HTML CSS files

---
+++ main.js
@@ -0,0 +1,52 @@
+/* var names = ['naveen', 'dinesh', 'santhosh', 'renga']
+
+switch( names[5] ){
+    case 'naveen' : console.log('Hey Naveen'); break;
+    case 'dinesh' : console.log('Hey Dinesh'); break;
+    case 'santhosh' : console.log('Hey Santhosh'); break;
+    case 'renga' : console.log('Hey Renga'); break;
+    default : console.log('can\'t find name'); break;
+}
+
+
+var green = 5
+var blue = 8
+
+if(green > blue){
+    console.log("1st statement is true");
+}
+else if (green < blue){
+    console.log("2nd statement is true")
+}
+else {
+    console.log("statement is false");
+} 
+
+
+for(var i = 0; i < 10; i++){
+    console.log(i)
+}
+
+
+var arr = ['Naveen', 'Dinesh', 'Santhosh', 'Renga']
+
+for(var i = 0; i < arr.length; i++){
+    console.log(arr[i])
+}
+
+
+var arr = ['Naveen', 'Dinesh', 'Santhosh', 'Renga']
+
+for(var i of arr) {
+    console.log(i);
+}
+
+var number = 0;
+var text = ''
+
+while (number < 10){
+    text += 'Now I\'m at ' + number + '\n';
+    number++;
+}
+
+console.log(text) */
